ab3c20eac9aba46e130ef487975a45f1245cb920
PALAK AGRAWAL <agrawalpalak6666@gmail.com>
2026-06-02 20:41:48 +0530

    Add files via upload

---
+++ Week_1/Day_2/maximum_average_subarray_1.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    double findMaxAverage(vector<int> &nums, int k)
+    {
+        double sum = 0;
+        for (int i = 0; i < k; i++)
+        {
+            sum += nums[i];
+        }
+
+        double ans = sum;
+
+        for (int j = k; j < nums.size(); j++)
+        {
+            sum = sum + nums[j] - nums[j - k];
+            ans = max(ans, sum);
+        }
+        return ans / k;
+    }
+};
