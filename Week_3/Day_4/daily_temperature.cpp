64a5d29eafe920145fd339e7738cceab18db0c8a
palak-agrawal-666 <agrawalpalak6666@gmail.com>
2026-06-18 15:07:15 +0530

    week3 day4 prob1

---
+++ Week_3/Day_4/daily_temperature.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temp)
+    {
+        stack<int> st;
+        int n = temp.size();
+        vector<int> ans(n, 0);
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+
+            while (!st.empty() && temp[st.top()] <= temp[i])
+            {
+                st.pop();
+            }
+            if (!st.empty())
+                ans[i] = st.top() - i;
+            st.push(i);
+        }
+        return ans;
+    }
+};
