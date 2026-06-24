5ebc7e7f670a954373a06ec29119428fd8b2fecb
palak-agrawal-666 <agrawalpalak6666@gmail.com>
2026-06-24 13:07:31 +0530

    week4 day3 prob1

--- Week_4/Day_2/level_order_traversal.cpp
+++ Week_4/Day_2/level_order_traversal.cpp
@@ -9,30 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *left) : val(x), left(left), left(left) {}
  * };
  */
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode *root)
-    {
-        vector<vector<int>> ans;
-        queue<TreeNode *> q;
-        if (root == NULL)
-            return ans;
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        queue<TreeNode*>q;
+        if(root==NULL) return ans;
         q.push(root);
 
-        while (!q.empty())
-        {
-            vector<int> level;
+        while(!q.empty()){
+            vector<int>level;
             int size = q.size();
 
-            for (int i = 0; i < size; i++)
-            {
-                TreeNode *front = q.front();
+            for(int i=0;i<size;i++){
+                TreeNode* front = q.front();
                 q.pop();
-                if (front->left != NULL)
-                    q.push(front->left);
-                if (front->right != NULL)
-                    q.push(front->right);
+                if(front->left!=NULL) q.push(front->left);
+                if(front->right!=NULL) q.push(front->right);
                 level.push_back(front->val);
             }
 
